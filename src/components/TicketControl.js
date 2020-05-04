49faa6cd3d8e51b54a10f508a7ba3d25b9ea7953
Keturah Howard <keturah.dev@gmail.com>
2020-05-03 21:56:17 -0700

    refactor to add update fucntionality form firestore

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -68,22 +68,12 @@ class TicketControl extends React.Component {
     this.setState({editing: true});
   }
 
-  // handleEditingTicketInList = (ticketToEdit) => {
-  //   const { dispatch } = this.props;
-  //   const { id, names, location, issue } = ticketToEdit;
-  //   const action = {
-  //     type: 'ADD_TICKET',
-  //     id: id,
-  //     names: names,
-  //     location: location,
-  //     issue: issue,
-  //   }
-  //   dispatch(action);
-  //   this.setState({
-  //     editing: false,
-  //     selectedTicket: null
-  //   });
-  // }
+  handleEditingTicketInList = () => {
+  this.setState({
+    editing: false,
+    selectedTicket: null
+  });
+}
 
   render(){
     let currentlyVisibleState = null;
